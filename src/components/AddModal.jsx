2f6359598fd10cefc609c49b48f351cd3d232b1a
Mucahit <mucahit.kartal2015@gmail.com>
2023-12-11 20:59:02 +0300

    first and added modal

---
+++ src/components/AddModal.jsx
@@ -0,0 +1,39 @@
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+
+import Form from "react-bootstrap/Form";
+
+function AddModal({ show, handleClose }) {
+  return (
+    <>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="name">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="text" placeholder="Enter name" required />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="date">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="datetime-local" placeholder="Date" required />
+            </Form.Group>
+            <div className="text-center">
+              <Button variant="success" type="submit" className="me-2">
+                Save
+              </Button>
+              <Button variant="danger" type="button" onClick={handleClose}>
+                Close
+              </Button>
+            </div>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+}
+
+export default AddModal;
